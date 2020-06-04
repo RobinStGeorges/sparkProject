34cd6da1356c20068489d43ecf330159fd9f92f8
Kireta <robin.sgeorges@gmail.com>
2020-06-04 15:48:23 +0200

    added funct to filter col and filter by date

--- src/FootballApp.py
+++ src/FootballApp.py
@@ -15,7 +15,7 @@ def main(argv):
     df = load_csv_into_df()
     formated_df = format_df_data(df)
     filtered_df = filter_col_data(formated_df)
-    #filtered_df.show()
+    filtered_df.show()
 
 
 # Import data from CSV into a Spark dataframe
@@ -23,16 +23,20 @@ def load_csv_into_df():
     df = spark.read.csv("src/df_matches.csv", header=True)
     return df
 
-
+#Format data with correct name and change NULL val to 0
 def format_df_data(df):
     df = df.withColumnRenamed('X4', 'match').withColumnRenamed('X6', 'competition')
-    casted_df = df.withColumn("penalty_france", df.penalty_france.cast('int')).withColumn("penalty_adversaire", df.penalty_adversaire.cast('int'))
+    casted_df = df.withColumn("penalty_france", df.penalty_france.cast('int'))\
+        .withColumn("penalty_adversaire", df.penalty_adversaire.cast('int'))\
+        .withColumn("score_adversaire", df.score_adversaire.cast('int'))\
+        .withColumn("score_france", df.score_france.cast('int'))
     return casted_df.na.fill(0)
 
+#keep only usefull col from df and with date > mars 1980
 def filter_col_data(df):
     filtered_df = df.select('match', 'competition', 'adversaire', 'score_france', 'score_adversaire', 'penalty_france', 'penalty_adversaire', 'date')
-    filtered_df.show()
-    return filtered_df
+    filtered_by_date_df = filtered_df.filter(filtered_df.date > '1980-03-01')
+    return filtered_by_date_df
 
 
 if __name__ == "__main__":
