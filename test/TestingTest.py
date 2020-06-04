822864250e1bc4392181f2fd67a4b5e6a8e1a083
Kireta <robin.sgeorges@gmail.com>
2020-06-04 14:38:31 +0200

    added test class

---
+++ test/TestingTest.py
@@ -0,0 +1,7 @@
+import org.apache.spark.sql.SparkSession
+import fr.xebia.training.advancedspark.testing.Testing.getMaxFromColumn
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestingTest extends FlatSpec with Matchers {
+
+}
