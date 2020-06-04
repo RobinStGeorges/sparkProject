429b39f506d023623b1451c8e9468d690d1757f3
Kireta <robin.sgeorges@gmail.com>
2020-06-04 15:20:56 +0200

    filtered the useful col

--- src/FootballApp.py
+++ src/FootballApp.py
@@ -14,7 +14,8 @@ def main(argv):
     print(argv[1])
     df = load_csv_into_df()
     formated_df = format_df_data(df)
-    formated_df.show()
+    filtered_df = filter_col_data(formated_df)
+    #filtered_df.show()
 
 
 # Import data from CSV into a Spark dataframe
@@ -26,8 +27,12 @@ def load_csv_into_df():
 def format_df_data(df):
     df = df.withColumnRenamed('X4', 'match').withColumnRenamed('X6', 'competition')
     casted_df = df.withColumn("penalty_france", df.penalty_france.cast('int')).withColumn("penalty_adversaire", df.penalty_adversaire.cast('int'))
-    casted_df.show()
     return casted_df.na.fill(0)
+
+def filter_col_data(df):
+    filtered_df = df.select('match', 'competition', 'adversaire', 'score_france', 'score_adversaire', 'penalty_france', 'penalty_adversaire', 'date')
+    filtered_df.show()
+    return filtered_df
 
 
 if __name__ == "__main__":
