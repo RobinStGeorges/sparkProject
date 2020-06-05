bb54a50229b1e28f9471878a20c1d8f0518b94a6
Kireta <robin.sgeorges@gmail.com>
2020-06-05 17:06:58 +0200

    stat dataframe finished

--- src/FootballApp.py
+++ src/FootballApp.py
@@ -7,6 +7,7 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import StringType
 from pyspark.sql.window import Window
 from pyspark.sql.types import BooleanType
+from pyspark.sql.types import IntegerType
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -17,15 +18,16 @@ def main(argv):
     formated_df = format_df_data(df)
     filtered_df = filter_col_data(formated_df)
     df_with_is_played_home_col = add_column_is_played_home(filtered_df)
+    df_with_is_played_home_col.show()
     stats_df = get_stats_df(df_with_is_played_home_col)
-    stats_df.show()
-    #df_with_is_played_home_col.show()
+    stats_df.show(100)
+    # df_with_is_played_home_col.show()
 
 
 # Import data from CSV into a Spark dataframe
 def load_csv_into_df(file_path):
     df = spark.read.csv(file_path, header=True)
-    #df = spark.read.csv("src/df_matches.csv", header=True)
+    # df = spark.read.csv("src/df_matches.csv", header=True)
     return df
 
 
@@ -54,14 +56,24 @@ def is_played_home(rencontre):
         return False
 
 
+def is_world_cup(match_type):
+    if match_type[:5] == 'Coupe':
+        return 1
+    else:
+        return 0
 
-#UDF calling checking if match is played at home, returns a bool
+
+# UDF calling is_played_home() checking if match is played at home, returns a bool
 is_home_udf = F.udf(is_played_home, BooleanType())
+
+# UDF calling is_world_cup() checking if match is from a world cup
+is_world_cup_udf = F.udf(is_world_cup, IntegerType())
 
 
 def add_column_is_played_home(df):
     df_with_is_played_home_col = df.withColumn("is_played_home", is_home_udf(df.match))
     return df_with_is_played_home_col
+
 
 def get_stats_df(df):
     df_stat = (df
@@ -69,7 +81,12 @@ def get_stats_df(df):
         .agg(
         F.avg(df.score_france).alias("avg_score_france"),
         F.avg(df.score_adversaire).alias("avg_score_adversaire"),
-        F.count(df.adversaire).alias("nb_match_jou-e-accent-aigu")
+        F.count(df.adversaire).alias("nb_match_jou-e-accent-aigu"),
+        (F.sum(df.is_played_home.cast('int')) * 100 / F.count(df.adversaire)).alias('percent_home_played'),
+        F.sum(is_world_cup_udf(df.competition)).alias('nb_mach_in_world_cup'),
+        F.max(df.penalty_france).alias('max_penalty_france'),
+        (F.sum(df.penalty_france) - F.sum(df.penalty_adversaire)).alias('dif penalty')
+
     )
     )
     return df_stat
@@ -77,6 +94,7 @@ def get_stats_df(df):
 
 def write_df_to_parquet_file(df, file_name):
     df.write.parquet(file_name)
+
 
 if __name__ == "__main__":
     main(sys.argv)
